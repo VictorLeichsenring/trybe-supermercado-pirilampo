e85dbcb1824ce8f663a71bf808d1a100c1f452fb
Victor Leichsenring <victor.leichsenring@gmail.com>
2023-06-07 17:41:59 -0300

    Criação da função serchProductByName

--- src/6-search-product-by-name.js
+++ src/6-search-product-by-name.js
@@ -1,7 +1,21 @@
 const stockProducts = require('./data.json');
 
-const searchProductByName = () => {
+const searchProductByName = (searchName) => {
   // Desenvolva seu código dentro dessa função...
+  let productFound = {};
+  if (searchName == null) {
+    return null;
+  } else {
+    for (let i = 0; i < stockProducts.length; i += 1) {
+      if (searchName === stockProducts[i].productName) {
+        productFound.description = stockProducts[i].description;
+        productFound.formattedPrice = `R$ ${stockProducts[i].price.toFixed(2)}`;
+        return productFound;
+      }
+    }
+    if (Object.keys(productFound).length === 0) {
+      return null;
+    }
+  }
 };
-
 module.exports = { searchProductByName };
