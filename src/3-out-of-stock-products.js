2b93fc4de0c503a48cb7d4389721c24b684919ad
Victor Leichsenring <victor.leichsenring@gmail.com>
2023-06-07 15:55:01 -0300

    Criação da função getOutOfStockProducts

--- src/3-out-of-stock-products.js
+++ src/3-out-of-stock-products.js
@@ -2,6 +2,13 @@ const stockProducts = require('./data.json');
 
 const getOutOfStockProducts = () => {
   // Desenvolva seu código dentro dessa função...
+  let unavailableProducts = [];
+  for (let i = 0; i < stockProducts.length; i += 1) {
+    if (stockProducts[i].quantityInStock == 0) {
+      unavailableProducts.push(stockProducts[i].productName);
+    }
+  }
+  return unavailableProducts;
 };
 
 module.exports = { getOutOfStockProducts };
