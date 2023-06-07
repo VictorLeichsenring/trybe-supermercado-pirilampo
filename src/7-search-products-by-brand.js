4594b8ce895be71d421ca9825ce90f4a9859fe62
Victor Leichsenring <victor.leichsenring@gmail.com>
2023-06-07 17:55:27 -0300

    Criação da função searchProductsByBrand

--- src/7-search-products-by-brand.js
+++ src/7-search-products-by-brand.js
@@ -1,7 +1,18 @@
 const stockProducts = require('./data.json');
 
-const searchProductsByBrand = () => {
+const searchProductsByBrand = (brand) => {
   // Desenvolva seu código dentro dessa função...
+  let productsFound = [];
+  for (let i = 0; i < stockProducts.length; i += 1) {
+    if (brand === stockProducts[i].brand) {
+      let product = {
+        description: stockProducts[i].description,
+        formattedPrice: `R$ ${stockProducts[i].price.toFixed(2)}`,
+      };
+      productsFound.push(product);
+    }
+  }
+  return productsFound;
 };
 
 module.exports = { searchProductsByBrand };
