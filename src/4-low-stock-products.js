e601f0ae4bb30a4bb5b0c7b86d97dfc84b15bec0
Victor Leichsenring <victor.leichsenring@gmail.com>
2023-06-07 16:05:46 -0300

    Criação da função getLowStockProducts

--- src/4-low-stock-products.js
+++ src/4-low-stock-products.js
@@ -2,6 +2,14 @@ const stockProducts = require('./data.json');
 
 const getLowStockProducts = () => {
   // Desenvolva seu código dentro dessa função...
+  let scarceProducts = [];
+  for (let i = 0; i < stockProducts.length; i += 1) {
+    if (stockProducts[i].quantityInStock > 0 && stockProducts[i].quantityInStock <= 10) {
+      scarceProducts.push(`${stockProducts[i].productName}: ${stockProducts[i].quantityInStock
+      } unidades`)
+    }
+  }
+  return scarceProducts;
 };
 
 module.exports = { getLowStockProducts };
