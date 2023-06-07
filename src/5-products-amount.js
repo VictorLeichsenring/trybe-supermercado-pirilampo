0149d39296466c524f24cc408ee15605802f2b89
Victor Leichsenring <victor.leichsenring@gmail.com>
2023-06-07 16:15:10 -0300

    Criação da função getProductsAmount

--- src/5-products-amount.js
+++ src/5-products-amount.js
@@ -2,6 +2,11 @@ const stockProducts = require('./data.json');
 
 const getProductsAmount = () => {
   // Desenvolva seu código dentro dessa função...
+  let totalProducts = 0;
+  for (let i = 0; i < stockProducts.length; i += 1) {
+    totalProducts += stockProducts[i].quantityInStock;
+  }
+  return totalProducts;
 };
 
 module.exports = { getProductsAmount };
