9d9a9ee0115dec547ac806741d5ddea571d9f93f
Victor Leichsenring <victor.leichsenring@gmail.com>
2023-06-07 15:47:45 -0300

    Criação da função getUniqueProductsName

--- src/2-unique-products-name.js
+++ src/2-unique-products-name.js
@@ -2,6 +2,10 @@ const stockProducts = require('./data.json');
 
 const getUniqueProductsName = () => {
   // Desenvolva seu código dentro dessa função...
+  let productsList = [];
+  for (let i = 0; i < stockProducts.length; i += 1) {
+    productsList.push(stockProducts[i].productName);
+  }
+  return productsList;
 };
-
 module.exports = { getUniqueProductsName };
