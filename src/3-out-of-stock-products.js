18ec4d251fe5a44cf6b9c94218cb33b5ebea278d
Victor Leichsenring <victor.leichsenring@gmail.com>
2023-06-07 18:18:26 -0300

    Criação da função getProductOnSale

--- src/3-out-of-stock-products.js
+++ src/3-out-of-stock-products.js
@@ -4,9 +4,9 @@ const getOutOfStockProducts = () => {
   // Desenvolva seu código dentro dessa função...
   let unavailableProducts = [];
   for (let i = 0; i < stockProducts.length; i += 1) {
-    if (stockProducts[i].quantityInStock == 0) {
+    if (stockProducts[i].quantityInStock === 0) {
       unavailableProducts.push(stockProducts[i].productName);
-    }
+    };
   }
   return unavailableProducts;
 };
--- src/8-products-on-sale.js
+++ src/8-products-on-sale.js
@@ -2,6 +2,17 @@ const stockProducts = require('./data.json');
 
 const getProductsOnSale = () => {
   // Desenvolva seu código dentro dessa função...
+  let productsFound = [];
+  for (let i = 0; i < stockProducts.length; i += 1) {
+    if (stockProducts[i].onSale) {
+      let product = {
+        description: stockProducts[i].description,
+        formattedPrice: `R$ ${stockProducts[i].price.toFixed(2)}`,
+        onSale: stockProducts[i].onSale,
+      };
+      productsFound.push(product);
+    }
+  }
+  return productsFound;
 };
-
 module.exports = { getProductsOnSale };
